52d1d38e3595d9aaa6832e265be3c3bc0392e273
Dan Barua <danbarua@gmail.com>
2014-02-17 09:31:13 +0000

    refactor directory structure

---
+++ Koans/Lessons/WriteYourOwnLesson.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace Koans.Lessons
+{
+    
+    public class WriteYourOwnLesson
+    {
+        /*
+         * Find something we missed?
+         * Write it here, and email it to isidore@setgame.com 
+         */
+
+
+        [Fact]
+        public void TheMissingLesson()
+        {
+        }
+
+        public object ___ = "Please Fill in the blank";
+        public int ____ = 100;
+    }
+}
---
+++ Koans/Tests/Lesson4TimeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Koans.Utils;
+using Xunit;
+using CurrentLesson = Koans.Lessons.Lesson4Time;
+
+namespace Koans.Tests
+{
+	
+	public class Lesson4TimeTest
+	{
+		[Fact]
+		
+		public void TestAllQuestions()
+		{
+			//KoanUtils.Verify<CurrentLesson>(l => l.LaunchingAnActionInTheFuture(), 0);
+			//KoanUtils.Verify<CurrentLesson>(l => l.LaunchingAnEventInTheFuture(), 0);
+			KoanUtils.Verify<CurrentLesson>(l => l.YouCanPlaceATimelimitOnHowLongAnEventShouldTake(), 2100);
+			KoanUtils.Verify<CurrentLesson>(l => l.Throttling(), "from scott");
+			KoanUtils.Verify<CurrentLesson>(l => l.Buffering(), "Scott Reed");
+			KoanUtils.Verify<CurrentLesson>(l => l.TimeBetweenCalls(), "slow down");
+
+		}
+
+
+	}
+}
